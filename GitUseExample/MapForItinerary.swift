ed3093ba3c49aa62ce8d49122df4ef0879ac3fc2
Gabriel Zempoalteca <gabriel.zmpltc@gmail.com>
2018-06-15 18:03:59 -0500

    Cambio de controlador

--- GitUseExample/MapForItinerary.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MapForItinerary.swift
-//  GitUseExample
-//
-//  Created by Gabriel  on 15/06/18.
-//  Copyright © 2018 Zempoalteca. All rights reserved.
-//
-
-import Foundation
---
+++ GitUseExample/MapForItineraryVC.swift
@@ -0,0 +1,22 @@
+//
+//  MapForItineraryVC.swift
+//  GitUseExample
+//
+//  Created by Gabriel  on 15/06/18.
+//  Copyright © 2018 Zempoalteca. All rights reserved.
+//
+
+import UIKit
+
+class MapForItineraryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    func sayHello() {
+        print("Hello from MapForItinerary")
+    }
+}
